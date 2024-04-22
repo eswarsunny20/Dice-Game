3a967e82bb71135c9dac51a73f63a48e743e4635
eswarsunny20 <148360283+eswarsunny20@users.noreply.github.com>
2024-04-22 18:54:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function randomSelect() {
     var img = document.querySelector(".img1");
     var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    img.src = "images/dice" + randomNumber1 + ".png";
+    img.src = "dice" + randomNumber1 + ".png";
     var img2 = document.querySelector(".img2");
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-    img2.src = "images/dice" + randomNumber2 + ".png";
+    img2.src = "dice" + randomNumber2 + ".png";
     var title = document.querySelector("h1");
     if(randomNumber1 == randomNumber2){
         title.innerHTML = "Draw !";
